be89d3b598aa4cfae83db728989a65f8415f3948
nikita nikon <nikita.nikon@gmail.com>
2019-03-29 18:06:15 +0200

    nn: commit & push in order to trigger travis

--- spec/unit/version_spec.rb
+++ spec/unit/version_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 RSpec.describe Howitzer do
   it { expect(subject.constants).to include(:VERSION) }
+
   it 'should contains VERSION constant with correct format' do
     expect(Howitzer::VERSION).to match(/^\d+\.\d+(\.\d+)?$/)
   end
